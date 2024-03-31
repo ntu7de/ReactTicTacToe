9bcab54f83087c88a6e888d217ed734fdc4f5d54
Patrick Yuson <pyuson@patricks-mbp.myfiosgateway.com>
2024-03-30 22:55:03 -0400

    taking a pause after setting up handleClick() function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
+import { useState } from 'react';
+
 // value is a PROP
-function Square({ value }) {
-  return <button className="square">{value}</button>
+function Square() {
+  // Add a STATE so a component can "remember" it got clicked
+  const[value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('X');
+  }
+
+  return (
+    // When a square gets clicked 
+    <button 
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
