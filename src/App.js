7ebd417d08a9ecb8783a02489a67b64a6d282445
Patrick Yuson <pyuson@patricks-mbp.myfiosgateway.com>
2024-03-30 23:52:17 -0400

    Changed state tracking to the Board() function instead of Square()

--- src/App.js
+++ src/App.js
@@ -1,42 +1,29 @@
 import { useState } from 'react';
-
-// value is a PROP
-function Square() {
-  // Add a STATE so a component can "remember" it got clicked
-  const[value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue('X');
-  }
-
-  return (
-    // When a square gets clicked 
-    <button 
-      className="square"
-      onClick={handleClick}
-    >
-      {value}
-    </button>
-  );
+  
+function Square({value}) {
+  return <button className="square">{value}</button>;
 }
 
 export default function Board() {
+  const[squares, setSquares] = useState(Array(9).fill(null));
+
   return (
+    // Using the squares[] array helps us keep track of the square's state
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
